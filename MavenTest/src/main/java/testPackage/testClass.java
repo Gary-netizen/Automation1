33ed5e282ded7769c7e31583c05e6056b2bd7c95
Gaurav <Gaurav@192.168.0.102>
2019-12-23 12:02:00 +0530

    Test Comit

---
+++ MavenTest/src/main/java/testPackage/testClass.java
@@ -0,0 +1,15 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class testClass {
+
+	@Test(priority=1)
+	public void test() {
+		
+		System.out.println("hello world");
+	}
+	
+}
+
+
