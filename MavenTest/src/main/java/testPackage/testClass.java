a99a4bc90a88d7bf064ecf6a0ae37e5855004eb0
Gaurav <Gaurav@192.168.0.102>
2019-12-23 14:06:49 +0530

    TEST

--- MavenTest/src/main/java/testPackage/testClass.java
+++ MavenTest/src/main/java/testPackage/testClass.java
@@ -10,6 +10,18 @@ public class testClass {
 		System.out.println("hello world");
 	}
 	
+
+    @Test(priority=2)
+    public void arraySum() {
+	int [] arr= {5,5,4,23,6};
+	int sum=0;
+	int i;
+	
+	for (i=0;i<arr.length;i++) {
+		sum=sum+arr[i];
+		}
+	System.out.println("sum of array:"+sum);
+		
+	}
+	
 }
-
-
